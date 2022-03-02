7c40d46b53107a0f8f8b621290daf9648ece70c0
maysoonabed <maysoon.abdulqader99@gmail.com>
2022-03-02 09:25:22 +0200

    k

---
+++ src/Old/App.js
@@ -0,0 +1,26 @@
+import React from "react"
+import "./style.css"
+import { Routes, Route, Link } from "react-router-dom";
+import About from "./About"
+import Posts from "./Posts";
+
+
+const App = () => {
+
+    return (
+        <div>
+            <nav>
+                <Link to="/" className="navLink">Home</Link>
+                <Link to="/about" className="navLink">About</Link>
+            </nav>
+            <h1>Welcome to React Router!</h1>
+            <Routes>
+                <Route path="/" element={<Posts />} />
+                <Route path="about" element={<About />} />
+            </Routes>
+        </div>
+    );
+
+}
+
+export default App;
